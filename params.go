711799873ee849a64277b0c3546ec08fdbcee0d9
Nedyalko Dyakov <me@ndyakov.eu>
2015-02-28 00:21:47 +0200

    Add initial documentation of the Params methods.

--- params.go
+++ params.go
@@ -8,8 +8,19 @@ import (
 	"time"
 )
 
+// Params is a type on top of map[string]interface{}
+// that has a few useful getters and other methods.
+// This type can help you to work with json data that
+// can be unmarshalled into it and then extracted with
+// the appropriate type (it even has a getter for time.Time).
+// Also the Required and Empty methods may be helpful with
+// validating data. It is possible to work with nested Params,
+// getting an Interface that you can cast yourself to the
+// wanted type and adding new values to the Params structure.
 type Params map[string]interface{}
 
+// Add adds a new pair(key, value) to the Params structure.
+// Returns true if an existing value was overwritten.
 func (p Params) Add(key string, value interface{}) bool {
 	if fmt.Sprintf("%p", p) == fmt.Sprintf("%p", value) {
 		return false
@@ -20,6 +31,8 @@ func (p Params) Add(key string, value interface{}) bool {
 	return ok
 }
 
+// Empty checks if the Params structure is empty.
+// Returns false if a there are elements in the structure.
 func (p Params) Empty() bool {
 	for _ = range p {
 		return false
@@ -27,10 +40,16 @@ func (p Params) Empty() bool {
 	return true
 }
 
+// Remove deletes a key from the structure.
+// It won't fail even if called on a missing key.
 func (p Params) Remove(key string) {
 	delete(p, key)
 }
 
+// GetP will return Params structure if the value with
+// the specified key is of either map[string]interface{} or Params type.
+// Returns empty Params if the key is missing or if the value
+// was not one of the desired types.
 func (p Params) GetP(key string) Params {
 	if val, ok := p[key]; ok {
 		if vmap, ok := val.(map[string]interface{}); ok {
@@ -43,6 +62,9 @@ func (p Params) GetP(key string) Params {
 	return Params{}
 }
 
+// GetI will return the same as map[key] would have returned.
+// Returns an interface that you can try to cast to other types.
+// If there is no value with that key it will return nil.
 func (p Params) GetI(key string) interface{} {
 	if val, ok := p[key]; ok {
 		return val
@@ -51,6 +73,9 @@ func (p Params) GetI(key string) interface{} {
 	return nil
 }
 
+// Get returns a string representation of the value with
+// the specified key.
+// Returns empty string if there is no value with the provided key.
 func (p Params) Get(key string) string {
 	if val, ok := p[key]; ok {
 		return stringify(val)
@@ -59,6 +84,8 @@ func (p Params) Get(key string) string {
 	return ""
 }
 
+// GetString returns a string only if the value with the specified key
+// can be casted to string. Will return an empty string otherwise.
 func (p Params) GetString(key string) string {
 	if val, ok := p[key]; ok {
 		if vs, ok := val.(string); ok {
@@ -68,6 +95,8 @@ func (p Params) GetString(key string) string {
 	return ""
 }
 
+// GetInt parses the value with the provided key to an int.
+// If there is an error with the parsing, returns 0.
 func (p Params) GetInt(key string) int {
 	result, err := strconv.ParseInt(p.Get(key), 0, 0)
 	if err == nil {
@@ -77,6 +106,8 @@ func (p Params) GetInt(key string) int {
 	return 0
 }
 
+// GetInt8 parses the value with the provided key to an int8.
+// If there is an error with the parsing, returns 0.
 func (p Params) GetInt8(key string) int8 {
 	result, err := strconv.ParseInt(p.Get(key), 0, 8)
 	if err == nil {
@@ -86,6 +117,8 @@ func (p Params) GetInt8(key string) int8 {
 	return 0
 }
 
+// GetInt64 parses the value with the provided key to an int64.
+// If there is an error with the parsing, returns 0.
 func (p Params) GetInt64(key string) int64 {
 	result, err := strconv.ParseInt(p.Get(key), 0, 64)
 	if err == nil {
@@ -95,6 +128,8 @@ func (p Params) GetInt64(key string) int64 {
 	return 0
 }
 
+// GetFloat32 parses the value with the provided key to an float32.
+// If there is an error with the parsing, returns 0.
 func (p Params) GetFloat32(key string) float32 {
 	result, err := strconv.ParseFloat(p.Get(key), 32)
 	if err == nil {
@@ -104,6 +139,8 @@ func (p Params) GetFloat32(key string) float32 {
 	return 0
 }
 
+// GetFloat64 parses the value with the provided key to an float64.
+// If there is an error with the parsing, returns 0.
 func (p Params) GetFloat64(key string) float64 {
 	result, err := strconv.ParseFloat(p.Get(key), 64)
 	if err == nil {
@@ -113,10 +150,19 @@ func (p Params) GetFloat64(key string) float64 {
 	return 0
 }
 
+// GetFloat parses the value with the provided key to an float32.
+// If there is an error with the parsing, returns 0.
+// This is actually the same as calling GetFloat32.
 func (p Params) GetFloat(key string) float32 {
 	return p.GetFloat32(key)
 }
 
+// GetTime will return the value with the specified key
+// parsed as time.Time structure. The layout is time.RFC3339
+// or in other words the JSON format for the Date object in js.
+// The value should look like this:
+//     "2015-02-27T21:53:57.582Z"
+// Otherwise returns time.Time{}
 func (p Params) GetTime(key string) time.Time {
 	if result, err := time.Parse(time.RFC3339, p.Get(key)); err == nil {
 		return result
@@ -124,6 +170,9 @@ func (p Params) GetTime(key string) time.Time {
 	return time.Time{}
 }
 
+// GetSlice returns a slice of interface{} if the value
+// with the provided key can be casted to a slice.
+// Otherwise returns nil.
 func (p Params) GetSlice(key string) []interface{} {
 	if val, ok := p[key]; ok {
 		if val, ok := val.([]interface{}); ok {
@@ -134,6 +183,14 @@ func (p Params) GetSlice(key string) []interface{} {
 	return nil
 }
 
+// GetSliceStrings will return a slice of strings.
+// This slice of strings will be a result of casting the value
+// coresponding to the provided key to a slice of interface{}
+// and then individually casting each element to a string.
+// Only those elements that can be casted to a string will be
+// present in the final result. Those that cannot be casted to string
+// will be silently ignored. If the is not value with that
+// key or the value is not a slice, nil will be returned.
 func (p Params) GetSliceStrings(key string) []string {
 	var result []string
 	if val, ok := p[key]; ok {
@@ -150,6 +207,14 @@ func (p Params) GetSliceStrings(key string) []string {
 	return nil
 }
 
+// GetSliceInts will return a slice of strings.
+// This slice of strings will be a result of casting the value
+// coresponding to the provided key to a slice of interface{}
+// and then individually casting each element to an int.
+// Only those elements that can be casted to an int will be
+// present in the final result. Those who cannot be casted to int
+// will be silently ignored. If the is not value with that
+// key or the value is not a slice, nil will be returned.
 func (p Params) GetSliceInts(key string) []int {
 	var result []int
 	if val, ok := p[key]; ok {
@@ -166,7 +231,22 @@ func (p Params) GetSliceInts(key string) []int {
 	return nil
 }
 
-// Transform to map[string][]string
+// URLValues return the values in the Params structure
+// as url.Values that can be then used with packages as
+// gorilla`s schema or goji`s params. The schema and params
+// packages expects different keys for the nested values. For
+// this to be able to generate the expected keys you may need to
+// pass the prefix and the sufix of the nested key. If the prefix
+// is empty string, then the prefix will be set to dot (".") and
+// the sufix will be an empty string. This is the way that gorilla`s
+// schema expect the nested keys to be represented. Example:
+//     some_key.inner_key.last_key
+// This somewhat resembles the mongo notation of nested objects as well.
+// To be able to use this with params, you will need to pass an prefix "["
+// and sufix "]". The key from the previous example will now be:
+//     some_key[inner_key][last_key]
+//
+// If the Params structure is blank, then an empty url.Values will be returned.
 func (p Params) URLValues(prefix, sufix string) url.Values {
 	return p.toURLValues(p, prefix, sufix, false)
 }
@@ -212,7 +292,14 @@ func (p Params) toURLValues(set Params, prefix, sufix string, subParse bool) url
 	return result
 }
 
-// Validators
+// Required will return an error if one of the passed keys is missing
+// in the Params structure. As far as Required cares - an empty string
+// is the same as missing value.
+// To validate nested parameters please use the dotted notation:
+//     some_key.nested_key.last_key
+// Returns an error message of the following type:
+//     "the parameter {key} is required"
+// If all keys are present will return nil.
 func (p Params) Required(keys ...string) error {
 	for _, key := range keys {
 		if ok := p.exists(p, key); !ok {
